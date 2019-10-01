0919327591823b7643b3d5ed8e44fdbe7f381ef5
Perry Brandiezs <pbrandiezs@gmail.com>
2019-10-01 15:25:51 -0700

    feat: Add restaurant image alt name and title on main page

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
+  image.title = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
