ce3881262a828f4839b23e43a8e2dc3ac167ff85
Perry Brandiezs <pbrandiezs@gmail.com>
2019-10-03 15:51:33 -0700

    fix: Corrected spacing

--- sw.js
+++ sw.js
@@ -1,54 +1,54 @@
 var CACHE_NAME = 'restaurant-reviews-v1';
 var urlsToCache = [
-  '/',
-  'restaurant.html',
-  'css/styles.css',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/restaurant_info.js',
-  'data/restaurants.json'
+    '/',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json'
 ];
 
 /* install */
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
     );
-  });
+});
 
 /* activate */
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.delete(CACHE_NAME)
-  );
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.delete(CACHE_NAME)
+    );
 });
 
 /* fetch */
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            // Check if a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        var responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
     );
 });
