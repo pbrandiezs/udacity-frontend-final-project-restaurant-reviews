22584c6145ac75cc258566b13a4627a1648b02b1
Perry Brandiezs <pbrandiezs@gmail.com>
2019-10-14 09:45:35 -0700

    fix: Modify dbhelper.js to work with local files for hosting.  Changed index.html header and footer to point to index.html instead of /, for hosting as a sub-page on portfolio site.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -21,8 +21,9 @@ class DBHelper {
      * Change this to restaurants.json file location on your server.
      */
     static get DATABASE_URL() {
-        const port = 8000; // Change this to your server port
-        return `http://localhost:${port}/data/restaurants.json`;
+        // const port = 8000; // Change this to your server port
+        //return `http://localhost:${port}/data/restaurants.json`;
+        return `data/restaurants.json`;
     }
 
     /**
@@ -163,7 +164,7 @@ class DBHelper {
      * Restaurant image URL.
      */
     static imageUrlForRestaurant(restaurant) {
-        return (`/img/${restaurant.photograph}`);
+        return (`img/${restaurant.photograph}`);
     }
 
     /**
